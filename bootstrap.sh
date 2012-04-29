c367fcaac585ca6813c1e065606967be3f281d3a
Zach Turk <zachturk@gmail.com>
2012-04-28 22:02:26 -0700

    Modified the bootstrap file to support installation from any directory.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-endpath="$HOME/.VimSettings"
+endpath=$PWD
 
 warn() {
     echo "$1" >&2
@@ -11,16 +11,15 @@ die() {
     exit 1
 }
 
-echo "thanks for installing spf13-vim\n"
-
 # Backup existing .vim stuff
 echo "backing up current vim config\n"
 today=`date +%Y%m%d`
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv $i $i.$today; done
 
 
-echo "cloning spf13-vim\n"
-git clone --recursive -b 3.0 http://github.com/zachturk/VimSettings.git $endpath
+echo "cloning vim settings repo\n"
+#git clone --recursive -b 3.0 http://github.com/zachturk/VimSettings.git $endpath
+echo $endpath 
 mkdir -p $endpath/.vim/bundle
 ln -s $endpath/.vimrc $HOME/.vimrc
 ln -s $endpath/.vim $HOME/.vim
